d4d19953eb911800e3e2b5c7fdebe07f16e8bc42
Siddhant <bansal.siddhant@gmail.com>
2016-12-09 11:16:56 +0530

    Upgraded to Unity 5.5

--- Assets/Scripts/BallManager.cs
+++ Assets/Scripts/BallManager.cs
@@ -4,5 +4,12 @@ using System.Collections;
 public class BallManager : MonoBehaviour {
 
 	public bool hasEntered = false;
+    ParticleSystem sparks;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+            Instantiate(sparks, contact.point, Quaternion.identity);
+    }
 
 }
